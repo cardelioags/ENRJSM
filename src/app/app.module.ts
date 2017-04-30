8ec092e57eb40eb937b85cbeecf186b765072b99
David Casillas <David Casillas>
2017-04-29 23:14:15 -0500

    Version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterializeModule } from "angular2-materialize";
 
 import { AppComponent } from './app.component';
+import { LogginComponent } from './loggin/loggin.component';
+import { PrincipalViewComponent } from './principal-view/principal-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogginComponent,
+    PrincipalViewComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
